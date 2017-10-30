9c9f810f3a0851a32fa246515dc451b0fa471a88
Humberto Alves <hjalves@live.com>
2017-10-30 21:17:32 +0000

    Increase Song fields size

--- playlists_api/models.py
+++ playlists_api/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Song(models.Model):
-    title = models.CharField(max_length=50)
-    artist = models.CharField(max_length=50)
-    album = models.CharField(max_length=50, blank=True)
+    title = models.CharField(max_length=70)
+    artist = models.CharField(max_length=70)
+    album = models.CharField(max_length=70, blank=True)
 
     def __str__(self):
         return '%s - %s' % (self.artist, self.title)
