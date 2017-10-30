89c4d9e2b985a8e45f3a797e25c1c91e49c7df02
Humberto Alves <hjalves@live.com>
2017-10-30 11:33:52 +0000

    Compatibility with Python 3.5

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+import codecs
 import json
 import os
+from pathlib import Path
 import sys
 from urllib.request import urlopen, Request
 import unicodedata
@@ -9,8 +11,8 @@ import unicodedata
 import django
 
 # Add project root to $PYTHONPATH
-project_root = os.getcwd()
-sys.path.append(project_root)
+project_root = Path(__file__).parent.parent.absolute()
+sys.path.append(str(project_root))
 
 # Load Django environment (settings, apps, db...)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "playlists.settings")
@@ -25,9 +27,11 @@ from playlists_api.models import Song, User
 # freemusicarchive.org replies with a 403 if User-Agent is 'Python-urllib/3.6'
 headers = {'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
 request = Request('http://freemusicarchive.org/recent.json', headers=headers)
+reader = codecs.getreader("utf-8")
+
 
 with urlopen(request) as response:
-    data = json.load(response)
+    data = json.load(reader(response))
 
 songs = [(track['track_title'], track['album_title'], track['artist_name'])
          for track in data['aTracks']]
